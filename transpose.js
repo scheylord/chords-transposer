261c1de6ee1bba245915e9344282ebfbd2920584
Khanh-Lam Mai <khanh-lam.mai@bnf.fr>
2015-05-18 16:58:03 +0200

    original commit

---
+++ transpose.js
@@ -0,0 +1,14 @@
+﻿// A $( document ).ready() block.
+$( document ).ready(function() {
+	var chordLineRegex = /([\n\r\s]+[A-G](#|b)?(maj|m|sus)?[0-9]?[\n\r\s]+)+/i
+	var chordRegex = /[A-G]((#|b)?(maj|m|sus)?[0-9]?)/gi
+
+	
+	var lines = $('textarea#song').val().split('\n');
+	for(var i = 0;i < lines.length;i++){
+		if(lines[i].match(chordLineRegex)) {
+			lines[i] = lines[i].replace(chordRegex, "X$1")
+			console.log(lines[i])
+		}
+	}
+});
